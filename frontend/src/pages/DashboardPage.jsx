7ed7de418d3a1ddee6638d8544a68f3783a07ce9
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-09-25 15:30:19 +0530

    updated'

---
+++ frontend/src/pages/DashboardPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Home from "../components/Home.jsx";
+import AllInternships from "../components/AllInternships.jsx";
+
+const DashboardPage = () => {
+  return (
+    <div>
+      <Home />
+      <AllInternships />
+    </div>
+  );
+};
+
+export default DashboardPage;
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,14 +2,13 @@ import React from "react";
 import Navbar from "../components/Navbar.jsx";
 import Home from "../components/Home.jsx";
 import Footer from "../components/Footer.jsx";
-import AllInternships from "../components/AllInternships.jsx";
+import DashboardPage from "./DashboardPage.jsx";
 
 const HomePage = () => {
   return (
     <div className="flex flex-col items-center justify-center">
       <Navbar />
-      <Home />
-      <AllInternships />
+      <DashboardPage />
       <Footer />
     </div>
   );
