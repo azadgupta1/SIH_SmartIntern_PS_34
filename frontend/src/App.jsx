dddc163345c393599276d6a63ae72e891edd32d9
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-09-25 13:46:38 +0530

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RegistrationForm from "./pages/RegisterForm";
 import LoginForm from "./pages/LoginForm";
 import HomePage from "./pages/HomePage";
-import About from "./pages/About";
 
 export default function App() {
   return (
@@ -12,7 +11,6 @@ export default function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/register" element={<RegistrationForm />} />
         <Route path="/login" element={<LoginForm />} />
-        <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
   );
