0834f43856f508f8eb156ffd7650137f61d2b267
Bhoomi Jadhav <jadhavbhoomi16@gmail.com>
2025-09-25 15:36:08 +0530

    updated

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,21 +2,13 @@ import React from "react";
 import Navbar from "../components/Navbar.jsx";
 import Home from "../components/Home.jsx";
 import Footer from "../components/Footer.jsx";
-<<<<<<< Updated upstream
 import DashboardPage from "./DashboardPage.jsx";
-=======
-import LandingPage from "../components/LandingPage.jsx";
->>>>>>> Stashed changes
 
 const HomePage = () => {
   return (
     <div className="flex flex-col items-center justify-center">
       <Navbar />
-<<<<<<< Updated upstream
       <DashboardPage />
-=======
-      <LandingPage />
->>>>>>> Stashed changes
       <Footer />
     </div>
   );
