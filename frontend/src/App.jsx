f21daa89bdd3243f14838c49901e7b2987155dac
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-09-25 10:02:50 +0530

    Home Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,13 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RegistrationForm from "./components/RegisterForm";
 import LoginForm from "./components/LoginForm";
+import HomePage from "./pages/HomePage";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path="/"
-          element={<h1 className="bg-blue-800">Hello World</h1>}
-        />
+        <Route path="/" element={<HomePage />} />
         <Route path="/register" element={<RegistrationForm />} />
         <Route path="/login" element={<LoginForm />} />
       </Routes>
